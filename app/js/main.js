dc7ee3d0c82a00e6bd5ebd1b374daed6a80b3b0b
che_vladislav <64832996+VladislavChe@users.noreply.github.com>
2021-04-22 15:19:23 +0300

    fixed nav menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,25 @@
+/*Прилипание навигационного меню*/
+{
+  const nav = document.querySelector('.index-header__nav'),
+    content = document.querySelector('.index-header__content');
+  // создаем переменную с расстоянием над меню
+  const navOffset = nav.getBoundingClientRect().top + document.body.scrollTop;
+  // ловим событие скролла
+  window.addEventListener('scroll', function () {
+    // создаем переменную с колличеством проскроленных пикселей
+    const scrolled = window.scrollY;
+
+    // если разница проскроленных пикселей больше высоты меню
+    if (scrolled > navOffset) {
+      nav.classList.add('nav-fixed');
+      content.classList.add('content-fixed');
+      // если разница проскроленных пикселей меньше высоты меню
+    } else if (scrolled < navOffset) {
+      nav.classList.remove('nav-fixed');
+      content.classList.remove('content-fixed');
+    }
+  });
+}
 /*index.html*/
 {
   const index = document.getElementsByClassName('.index-header');
