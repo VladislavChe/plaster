caa71c059f1476ae7b596aa9057f5150657218dd
che_vladislav <64832996+VladislavChe@users.noreply.github.com>
2021-04-28 12:16:11 +0300

    add markup ripple

--- app/js/main.js
+++ app/js/main.js
@@ -1,25 +1,3 @@
-/*Прилипание навигационного меню*/
-{
-  const nav = document.querySelector('.index-header__nav'),
-    content = document.querySelector('.index-header__content');
-  // создаем переменную с расстоянием над меню
-  const navOffset = nav.getBoundingClientRect().top + document.body.scrollTop;
-  // ловим событие скролла
-  window.addEventListener('scroll', function () {
-    // создаем переменную с колличеством проскроленных пикселей
-    const scrolled = window.scrollY;
-
-    // если разница проскроленных пикселей больше высоты меню
-    if (scrolled > navOffset) {
-      nav.classList.add('nav-fixed');
-      content.classList.add('content-fixed');
-      // если разница проскроленных пикселей меньше высоты меню
-    } else if (scrolled < navOffset) {
-      nav.classList.remove('nav-fixed');
-      content.classList.remove('content-fixed');
-    }
-  });
-}
 /*index.html*/
 {
   const index = document.getElementsByClassName('.index-header');
@@ -27,6 +5,61 @@
   if (!index) {
     // console.log('da');
   } else {
-    // console.log('da');
+    /*Прилипание навигационного меню*/
+    {
+      window.onscroll = function () {
+        checkMarginToTop();
+      };
+      const nav = document.querySelector('.index-header__nav'),
+        content = document.querySelector('.index-header__content');
+      // создаем переменную с расстоянием над меню
+      const navOffset = nav.offsetTop;
+      // ловим событие скролла
+      function checkMarginToTop() {
+        if (window.pageYOffset > navOffset) {
+          nav.classList.add('nav-fixed');
+          content.classList.add('content-fixed');
+        } else {
+          nav.classList.remove('nav-fixed');
+          content.classList.remove('content-fixed');
+        }
+      }
+    }
+
+    /*Ripple hover btn*/
+    {
+      const ANIMATEDCLASSNAME = 'animated';
+      const ELEMENTS = document.querySelectorAll('.btn');
+      const ELEMENTS_SPAN = [];
+
+      ELEMENTS.forEach((element, index) => {
+        let addAnimation = false;
+        // Elements that contain the "FLASH" class, add a listener to remove
+        // animation-class when the animation ends
+        if (element.classList[1] == 'FLASH') {
+          element.addEventListener('animationend', (e) => {
+            element.classList.remove(ANIMATEDCLASSNAME);
+          });
+          addAnimation = true;
+        }
+
+        // If The span element for this element does not exist in the array, add it.
+        if (!ELEMENTS_SPAN[index])
+          ELEMENTS_SPAN[index] = element.querySelector('span');
+
+        element.addEventListener('mouseover', (e) => {
+          ELEMENTS_SPAN[index].style.left = e.pageX - element.offsetLeft + 'px';
+          ELEMENTS_SPAN[index].style.top = e.pageY - element.offsetTop + 'px';
+
+          // Add an animation-class to animate via CSS.
+          if (addAnimation) element.classList.add(ANIMATEDCLASSNAME);
+        });
+
+        element.addEventListener('mouseout', (e) => {
+          ELEMENTS_SPAN[index].style.left = e.pageX - element.offsetLeft + 'px';
+          ELEMENTS_SPAN[index].style.top = e.pageY - element.offsetTop + 'px';
+        });
+      });
+    }
   }
 }
